e7074d9ad91acc8d59f3c945d4cc50292a9e3912
Arun Thakkar <thakkar.arun@zensar.in>
2017-03-24 16:47:34 +0530

    New API Connection Manager Pod Created

---
+++ APIConnectionManager/APIConnectionManager/APIConnectionManager.h
@@ -0,0 +1,19 @@
+//
+//  APIConnectionManager.h
+//  APIConnectionManager
+//
+//  Created by Zensar on 24/03/17.
+//  Copyright © 2017 Zensar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIConnectionManager.
+FOUNDATION_EXPORT double APIConnectionManagerVersionNumber;
+
+//! Project version string for APIConnectionManager.
+FOUNDATION_EXPORT const unsigned char APIConnectionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIConnectionManager/PublicHeader.h>
+
+
